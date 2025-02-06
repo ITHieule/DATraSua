a6dd16fe59859a480ca5d70e53c6a1b2b9bf508c
ITHieule <le0963845867@gmail.com>
2025-02-06 08:10:43 +0700

    addBaseSizes

---
+++ internal/api/controllers/baseSizes_controller.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"net/http"
+	"web-api/internal/api/services"
+	"web-api/internal/pkg/models/request"
+	"web-api/internal/pkg/models/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BaseSizesController struct {
+	*BaseController
+}
+
+var BaseSizes = &BaseSizesController{}
+
+func (c *BaseSizesController) AddBaseSizes(ctx *gin.Context) {
+	var requestParams request.BaseSizesrequest
+
+	if err := c.ValidateReqParams(ctx, &requestParams); err != nil {
+		response.FailWithDetailed(ctx, http.StatusBadRequest, nil, err.Error())
+		return
+	}
+	result, err := services.OrderBaseSizes.BaseSizesSevice(&requestParams)
+	if err != nil {
+		response.FailWithDetailed(ctx, http.StatusInternalServerError, nil, err.Error())
+		return
+	}
+	response.OkWithData(ctx, result)
+}
